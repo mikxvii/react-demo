7ebbd57fb67769d0cefb29ade7d53759ece02eb0
Mike Guerrero <mikxvii@berkeley.edu>
2025-04-19 21:57:31 -0700

    html elements

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ import { Profile } from './components/auth/Profile';
 import { List } from './components/generics/List';
 import { RandomNumber } from './components/restriction/RandomNumber'
 import { Toast } from './components/templateliterals/Toast'
+import { CustomButton } from './components/html/Button'
 
 // Entry point in react app
 function App() {
@@ -132,6 +133,10 @@ function App() {
       <RandomNumber value={10} isPositive />
 
       <Toast position='center'/>
+
+      <CustomButton variant='primary' onClick={() => console.log('Clicked')}>
+        Primary Button
+      </CustomButton>
     </div>
   ); 
 }
---
+++ src/components/html/Input.tsx
@@ -0,0 +1,5 @@
+type InputProps = React.ComponentProps<'input'>
+
+export const CustomInout = (props: InputProps) => {
+    return <input {...props} />
+}
