23454b1bdbf026dade2def47ddf3868be9534559
Mike Guerrero <mikxvii@berkeley.edu>
2025-04-19 17:06:33 -0700

    finished with useState

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -8,22 +8,22 @@ type AuthUser = {
 export const User = () => {
     // allows for future values to be added to the user object
     // without having to change the type of the user object
-    const [user, setUser] = useState<AuthUser| null>(null)
+    const [user, setUser] = useState<AuthUser>({} as AuthUser)
     const handleLogin = () => {
         setUser({
             name: 'Mike', 
             email: 'mikxvii@berkeley.edu'
         })
     }
-    const handleLogout = () => {
-        setUser(null)
-    }
+    // const handleLogout = () => {
+    //     setUser({} as AuthUser)})
+    // }
     return (
         <div>
             <button onClick={handleLogin}>Login</button>
-            <button onClick={handleLogout}>Logout</button>
-            <div>User name is {user?.name}</div>
-            <div>User email is {user?.email}</div>
+            {/* <button onClick={handleLogout}>Logout</button> */}
+            <div>User name is {user.name}</div>
+            <div>User email is {user.email}</div>
         </div>
     )
 }
